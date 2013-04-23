34530a113f85f14dd1ffd21f1b5b8a2bf872195c
Stephen Moss <bladexr8@gmail.com>
2013-04-23 20:35:23 +1000

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  
+  # Rails built in method to require presence of a password,
+  # require that they match, and add an authenticate method
+  # to compare an encrypted password to the password_digest
+  # to authenticate users
+  has_secure_password
   
   #ensure email uniqueness by converting email address to lower case
   # using before_save callback
@@ -9,4 +15,8 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
               uniqueness: { case_sensitive: false }
+              
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  
 end
